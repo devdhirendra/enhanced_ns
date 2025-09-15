bbe7c45e4cc081dfb74069d748a993573820eea3
Dhirendra Kumar Ojha <dhirendra.ojha@comviva.com>
2025-09-15 23:22:30 +0530

    init13

--- app/admin/staff/page.tsx
+++ app/admin/staff/page.tsx
@@ -262,7 +262,7 @@ export default function StaffPage() {
 
   return (
     <DashboardLayout title="Admin Settings" description="System configuration and user management">
-      <div className="space-y-4 sm:space-y-6 p-2 sm:p-4 lg:p-2">
+      <div className="space-y-4 sm:space-y-6 p-2 sm:p-4 lg:p-2  ">
         {/* Stats Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-4 lg:gap-6">
           {loading ? (
